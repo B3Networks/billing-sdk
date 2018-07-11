ae4968b2d722a9de5d1de86f037d11cbac09dd72
nvthinh <hoiio@Thinhs-MacBook-Pro.local>
2018-07-11 15:23:33 +0700

    modified for jusnita

--- src/main/java/sdk/CSVService.java
+++ src/main/java/sdk/CSVService.java
@@ -32,38 +32,16 @@ public class CSVService {
             ColumnPositionMappingStrategy mappingStrategy = 
             		new ColumnPositionMappingStrategy();
             mappingStrategy.setType(Entry.class);
-            String[] columns = new String[]{"orgName","type","invoiceText","startOn","cost","currency","qty"};
+            String[] columns = new String[]{"orgName","dest","source","answerTime","endTime"
+            ,"durationInSecond"};
             mappingStrategy.setColumnMapping(columns);
             CSVWriter csvWriter = new CSVWriter(writer, ',');
-
+            bc.write(mappingStrategy,csvWriter,list);
             
-            Map<String,Long> grp = list.stream().collect(
-                Collectors.groupingBy(
-                    Entry::getUnique, Collectors.counting()
-                )
-            );
-            
-            List<Entry> newlist = new ArrayList<Entry>();
-            for (String key : grp.keySet()){
-                //iterate over keys
-                System.out.println(key+" "+grp.get(key));
-                Entry a = list.stream()
-                .filter(x -> x.getUnique().equalsIgnoreCase(key))
-                .findFirst()
-                .get();
-                a.qty=grp.get(key);
-                newlist.add(a);
-            }
-        
-            bc.write(mappingStrategy,csvWriter,newlist);
-            double sum = newlist.stream().mapToDouble(Entry::getCost).sum();
-            CSVWriter cwriter2 = new CSVWriter(writer, ',');
-            String[] entries = {"Total", sum+""};
-            cwriter2.writeNext(entries); 
             
             writer.close();
         } catch (Exception ex){
-            System.out.println(ex);
+            ex.printStackTrace();
         }
     }
 
--- src/main/java/sdk/Entry.java
+++ src/main/java/sdk/Entry.java
@@ -4,6 +4,9 @@ package sdk;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.Duration;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Entry {
@@ -17,6 +20,11 @@ public class Entry {
     public String orgCode;
     public String currency;
     public double cost;
+    public double durationInSecond;
+    public String answerTime;
+    public String endTime;
+    public String dest;
+    public String source;
     public double costRate;
     public String invoiceText;
     public String startOn;
@@ -44,6 +52,37 @@ public class Entry {
 
     public String getStartOn(){
         return this.startOn;
+    }
+    public static boolean isNullOrBlank(String param) { 
+        return param == null || param.trim().length() == 0;
+    }
+
+    public double getDurationInSecond(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        if (isNullOrBlank(this.answerTime)) return 0;
+        if (isNullOrBlank(this.endTime)) return 0;
+        LocalDateTime startdate = LocalDateTime.parse(this.answerTime,
+         formatter);
+        LocalDateTime enddate = LocalDateTime.parse(this.endTime,
+         formatter);
+        Duration dur = Duration.between(startdate, enddate);
+
+        return dur.getSeconds();
+    }
+
+    public String getDest(){
+        return this.dest;
+    }
+    public String getSource(){
+        return this.source;
+    }
+
+    public String getAnswerTime(){
+        return this.answerTime;
+    }
+
+    public String getEndTime(){
+        return this.endTime;
     }
 
     public double getCost(){
